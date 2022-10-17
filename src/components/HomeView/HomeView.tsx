9f8692175f26b8fdd6829b2b4330c63eebf9d7ea
Ali Booresh <alibooresh1936@gmail.com>
2022-10-17 19:31:49 +0330

    Menu Icons, social media link, and profile added;

--- src/components/HomeView/HomeView.tsx
+++ src/components/HomeView/HomeView.tsx
@@ -1,7 +1,19 @@
 import { Fragment } from "react";
 import "../../style/Theme.css";
-import { Grid } from "@mui/material";
-
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import WorkHistoryOutlinedIcon from "@mui/icons-material/WorkHistoryOutlined";
+import AssignmentIndOutlinedIcon from "@mui/icons-material/AssignmentIndOutlined";
+import { Avatar, Grid, ListItem, ListItemButton, ListItemIcon,ListItemText} from "@mui/material";
+import List from "@mui/material/List";
+import IconButton from "@mui/material/IconButton";
+import './avatar.png';
 const HomeView = () => {
   return (
     <Fragment>
@@ -13,13 +25,88 @@ const HomeView = () => {
       >
         <Grid className="menu" item xs={3}>
           <Grid container direction="column">
-            <Grid className="info" item xs={3}>Profile</Grid>
-            <Grid className="social-links" item xs={2}>Social Media</Grid>
-            <Grid className="menu-links" item xs={7}>Menu</Grid>
+            <Grid className="info center" item xs={4}>
+              <List>
+                <ListItem className="center">
+                  <Avatar
+                    sx={{ width: "80px", height: "80px" }}
+                    src="./avatar.png"
+                  ></Avatar>
+                </ListItem>
+                <ListItem className="center">
+                  <ListItemText className="text" primary="Ali Booresh" />
+                </ListItem>
+                <ListItem className="social-links center">
+                  <List>
+                    <ListItem>
+                      <IconButton href="www.twitter.com/alibooresh">
+                        <TwitterIcon className="text social-icon" />
+                      </IconButton>
+                      <IconButton>
+                        <InstagramIcon className="text social-icon" />
+                      </IconButton>
+                      <IconButton>
+                        <FacebookIcon className="text social-icon" />
+                      </IconButton>
+                      <IconButton>
+                        <LinkedInIcon className="text socialicon" />
+                      </IconButton>
+                      <IconButton>
+                        <GitHubIcon className="text social-icon" />
+                      </IconButton>
+                    </ListItem>
+                  </List>
+                </ListItem>
+              </List>
+            </Grid>
+            <Grid className="menu-links" item xs={8}>
+              <List>
+                <ListItem>
+                  <ListItemButton className="menu-button">
+                    <ListItemIcon>
+                      <HomeOutlinedIcon className="text menu-icon" />
+                    </ListItemIcon>
+                    <ListItemText className="text" primary="Home" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem>
+                  <ListItemButton className="menu-button">
+                    <ListItemIcon>
+                      <PersonOutlineOutlinedIcon className="text menu-icon" />
+                    </ListItemIcon>
+                    <ListItemText className="text" primary="About" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem>
+                  <ListItemButton className="menu-button">
+                    <ListItemIcon>
+                      <AssignmentIndOutlinedIcon className="text menu-icon" />
+                    </ListItemIcon>
+                    <ListItemText className="text" primary="Resume" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem>
+                  <ListItemButton className="menu-button">
+                    <ListItemIcon>
+                      <WorkHistoryOutlinedIcon className="text menu-icon" />
+                    </ListItemIcon>
+                    <ListItemText className="text" primary="Portfilio" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem>
+                  <ListItemButton className="menu-button">
+                    <ListItemIcon>
+                      <EmailOutlinedIcon className="text menu-icon" />
+                    </ListItemIcon>
+                    <ListItemText className="text" primary="Contact" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Grid>
           </Grid>
         </Grid>
         <Grid className="box content" item xs={9}>
-          Content
+          
         </Grid>
       </Grid>
     </Fragment>
